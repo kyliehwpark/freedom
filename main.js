ed2e4aaeff23876b1b73b9fc557e05ba5756869f
Kylie Heewon Park <100271097+kyliehwpark@users.noreply.github.com>
2022-05-01 13:34:54 -0500

    Update main.js

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ const red = document.querySelector('#red')
 function changeDiv () {
   red.style.top = '108px'
   red.style.left = 'calc(10vw - 108px)'
-  red.style.backgroundColor = 'green'
+  red.style.backgroundColor = 'red'
 }
 
 red.addEventListener('click', changeDiv)
