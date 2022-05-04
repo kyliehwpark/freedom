6b7d409e73e80d126581aa10b4bd6b4d0105872a
Kylie Heewon Park <100271097+kyliehwpark@users.noreply.github.com>
2022-05-04 12:44:01 -0500

    Update main.js

--- main.js
+++ main.js
@@ -3,11 +3,6 @@ alert('freedom, in any case, is only possible by constantly struggling for it.')
 const red = document.querySelector('#red')
 
 function changeDiv () {
-  red.style.top = '108px'
-  red.style.left = 'calc(10vw - 108px)'
-  red.style.backgroundColor = 'red'
-}
-function red () {
     red.location.href = "dot.html";
 
 red.addEventListener('click', changeDiv)
