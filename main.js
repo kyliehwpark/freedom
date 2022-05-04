8375e284d383921040a17cde61fd78d7ca33025d
Kylie Heewon Park <100271097+kyliehwpark@users.noreply.github.com>
2022-05-04 13:00:12 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,10 +1,8 @@
 alert('freedom, in any case, is only possible by constantly struggling for it.')
 
-const red = document.querySelector('#red')
 
 function navToDot() {
     window.location.href = "hhttps://kyliehwpark.github.io/freedom/dot.html";
 }
 
 
-red.addEventListener('click', changeDiv)
